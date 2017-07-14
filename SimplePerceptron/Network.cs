e41e934e4a44179fe387fb12514f1456cb823079
MintCarasique <mykew98@gmail.com>
2017-07-14 15:19:44 +0300

    Initial commit

---
+++ SimplePerceptron/Network.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePerceptron
+{
+    class Network
+    {
+        public int[,] weight;
+        public int[,] mul;
+        public int[,] input;
+        public int sum;
+        public int limit = 9;
+    }
+}
